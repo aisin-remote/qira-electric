e301cc2c218222e95dffa5b2d2d7286a482fa485
sonya88 <sonyasinaga071003@gmail.com>
2024-01-22 13:12:47 +0700

    Test

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -1,3 +1,4 @@
+{{-- test --}}
 <x-perfect-scrollbar as="nav" aria-label="main" class="flex flex-col flex-1 gap-4 px-3">
 
     <x-sidebar.link title="Dashboard" href="{{ route('dashboard') }}" :isActive="request()->routeIs('dashboard')">
